38a2dc09454e9e01f06f2076f35e8012ee2d1158
Darkron98 <jprivillas98@gmail.com>
2022-06-04 01:49:44 -0500

    se implementaron un monton de cosas
    saefknj;sdfknjdsgfknjlsdf

--- mas_banderas/lib/providers/country_provider.dart
+++ mas_banderas/lib/providers/country_provider.dart
@@ -4,19 +4,25 @@ import 'package:http/http.dart' as http;
 import 'package:mas_banderas/models/country_model.dart';
 
 class CountryProvider extends ChangeNotifier {
-  String _country = 'colombia';
   String _domain = 'restcountries.com';
-  CountryModel? country;
 
-  MovieProvider() {
+  CountryProvider() {
     this.displayCountry();
   }
 
-  displayCountry() async {
-    final url = Uri.https(_domain, 'v3.1/name/$_country');
+  Future<String> _getJsonData() async {
+    final url = Uri.parse('https://restcountries.com/v3.1/name/colombia');
     final response = await http.get(url);
-    country = CountryModel.fromJson(response.body);
-    print('1');
+    final data = jsonDecode(response.body).toString();
+    final data2 = json.decode(response.body);
+    print(data2.runtimeType);
+    return (response.body);
+  }
+
+  displayCountry() async {
+    final jasonData = await _getJsonData();
+    //final response = CountryModel.fromJson(jasonData);
     notifyListeners();
+    //return response;
   }
 }
